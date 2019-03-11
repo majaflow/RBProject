c75b6d3dc95a428078e6760b85292d1d9ae0007d
Matthew Flowers <mjflowers5000@gmail.com>
2019-03-11 16:09:33 -0400

    file skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PostCommentsComponent } from './components/post-comments/post-comments.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PostCommentsComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/services/post-service.service.ts
@@ -0,0 +1,12 @@
+/*
+This service is intended to handle BOTH posts and comments most of our fetches should be here
+*/
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  constructor() { }
+}
